cb075e71ff68fb2f6de703fb7af19f460e42040d
aingez <aingthawan@gmail.com>
2023-07-21 13:56:26 +0700

    update whoami page details

--- my-app/src/Pages/whoami.js
+++ my-app/src/Pages/whoami.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import './whoami.css'
 
-export default function Cert() {
+export default function Me() {
     return (
         <div className="me">
             <div className="me-block">
-                <h1 className='me-title'>Who am I?</h1>
-                <h2 className='me-subtitle'>I am a ...</h2>
+                <h1 className='me-title'>Hello!<br />สวัสดี!</h1>
+                {/* <h2 className='me-subtitle'>Who Am I?</h2> */}
                     <p className='me-details'>
-                        I am a bla bla bla bla
-                    </p>
-                <h2 className='me-subtitle'>I am a ...</h2>
-                    <p className='me-details'>
-                        I am a bla bla bla bla
+                        <li>
+                            Hi! I'm Aing, from Bangkok, Thailand.
+                        </li>
+                        <li>
+                            A guy who is a professional dreamer and dedicates his life to his passion as a maker.
+                        </li>
+                        <li>
+                            I graduated in Pre-Electrical Engineering with Vocational Certificates, from KMUTNB.
+                        </li>
+                        <li>
+                            Currently studying for a Bachelor's Degree in Computer Engineering, at KMUTNB.
+                        </li>
+                        <li>
+                            Due to my curiosity, I love to learn new things and try to do everything by myself.
+                        </li>
+                        <li>
+                            I always interested in something that sustainable and eco-friendly or not reqire battery replace.
+                        </li>
+                        <li>
+                            I'm try to travel more, especially in hiking and camping.
+                        </li>
                     </p>
             </div>
         </div>
